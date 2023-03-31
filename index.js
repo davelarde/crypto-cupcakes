f9c7456e18f21311d037763912e1b00c4ea1ed8b
davelard <davelard@cisco.com>
2023-03-31 17:07:54 -0400

    final

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-require('dotenv').config('.env');
 const cors = require('cors');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const { PORT = 3000 } = process.env;
-// TODO - require express-openid-connect and destructure auth from it
 
 const { User, Cupcake } = require('./db');
-
 // middleware
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+// TODO - require express-openid-connect and destructure auth from it
+
+const { auth } = require("express-openid-connect");
+require('dotenv').config();
+
+
+
 
 /* *********** YOUR CODE HERE *********** */
 // follow the module instructions: destructure config environment variables from process.env
@@ -20,16 +23,67 @@ app.use(express.urlencoded({extended:true}));
   // define the config object
   // attach Auth0 OIDC auth router
   // create a GET / route handler that sends back Logged in or Logged out
+ 
+  const {
+    PORT,
+    AUTH0_SECRET,  // generate one by using: `openssl rand -base64 32`
+    AUTH0_BASE_URL,
+    AUTH0_CLIENT_ID,
+    AUTH0_ISSUER_BASE_URL,
+  } = process.env;
+  
+  
+  const config = {
+    authRequired: true, // this is different from the documentation
+    auth0Logout: true,
+    secret: AUTH0_SECRET,
+    baseURL:AUTH0_BASE_URL ,
+    clientID: AUTH0_CLIENT_ID,
+    issuerBaseURL: AUTH0_ISSUER_BASE_URL,
+  };
+  app.use(auth(config));
 
-app.get('/cupcakes', async (req, res, next) => {
-  try {
-    const cupcakes = await Cupcake.findAll();
-    res.send(cupcakes);
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-});
+  app.get('/',  (req, res) => {
+
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+     
+  });
+
+  app.use(async(req,res,next)=>{
+    try{
+      const [user] = await User.findOrCreate({
+        where:{
+          where:{name:user},
+          defaults:{
+            email: 'email',
+            name: 'username'
+          }
+        }
+      })
+      if(created){
+        console.log(user.name);
+        res.send(user.name);
+        console.log(user.email);
+        res.send(user.email)
+      }
+    }
+    catch(error){
+      console.log(error)
+      next()
+    }
+  })
+
+    
+  app.get('/cupcakes', async (req, res, next) => {
+    try {
+      const cupcakes = await Cupcake.findAll();
+      res.send(cupcakes);
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  });
+
 
 // error handling middleware
 app.use((error, req, res, next) => {
